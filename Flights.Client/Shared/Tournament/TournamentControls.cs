9fdbd63a8b5a93dc4e818b8b7483f3bb0a557ba4
Hanjo <hanjos.brainwash@googlemail.com>
2025-02-24 22:36:56 +0100

    refactored tournament controls

---
+++ Flights.Client/Shared/Tournament/TournamentControls.cs
@@ -0,0 +1,18 @@
+using Flights.Client.Shared.Game;
+using Flights.Domain.State;
+
+namespace Flights.Client.Shared.Tournament;
+
+public class TournamentControls
+{
+    public required DisplayType DisplayType { get; set; }
+    
+    public required TournamentState CurrentTournament {get;set;}
+    
+    public required Func<Guid, Task>? SwitchPlayerOrder { get; set; }
+    
+    public required Func<Guid, Task>? DevFinishGame { get; set; }
+    
+    public required Func<Task>? SkipLosersCup { get; set; }
+
+}
