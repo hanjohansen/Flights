fa5ee2e109b5826871bd3ce8d4ad416e4256f264
Hanjo <hanjos.brainwash@googlemail.com>
2025-11-09 21:17:49 +0100

    cleanup helper script

--- Flights.Client/CreateMigration.sh
+++ Flights.Client/CreateMigration.sh
@@ -3,14 +3,14 @@ echo Enter migration name:
 
 read migName
 
-
+echo "--------------------------------------------------------------------------------------------------------"
 command="dotnet ef migrations add ${migName} --project ../Flights.Storage/Flights.Storage.Sqlite/Flights.Storage.Sqlite.csproj --context FlightsDbContext -- --Storage:DbProvider Sqlite --Storage:ConnectionString 'DataSource=app.db;Cache=Shared;foreign keys=true'"
 echo "Sqlite -------------------------------------------------------------------------------------------------"
 echo "${command}"
 echo "--------------------------------------------------------------------------------------------------------"
 echo ""
 echo "Mysql -------------------------------------------------------------------------------------------------"
-command="/usr/lib/dotnet/sdk/9.0.111/dotnet ef migrations add ${migName} --project ../Flights.Storage/Flights.Storage.MySql/Flights.Storage.MySql.csproj --context FlightsDbContext -- --Storage:DbProvider MySql --Storage:ConnectionString 'Server=192.168.170.110;Port=3306;Database=flights;Uid=flights;Pwd=flights;'"
+command="dotnet ef migrations add ${migName} --project ../Flights.Storage/Flights.Storage.MySql/Flights.Storage.MySql.csproj --context FlightsDbContext -- --Storage:DbProvider MySql --Storage:ConnectionString 'Server=192.168.170.110;Port=3306;Database=flights;Uid=flights;Pwd=flights;'"
 echo "${command}"
 echo "--------------------------------------------------------------------------------------------------------"
 eval ${command}
