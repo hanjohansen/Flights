1a924c8c0b6325a39a8830adca38e29083f399c5
Hanjo <hanjos.brainwash@googlemail.com>
2025-12-03 17:32:12 +0100

    remove dispose on clock

--- Flights.Client/wwwroot/js/clock.js
+++ Flights.Client/wwwroot/js/clock.js
@@ -1,17 +1,9 @@
-let intervalId = -1;
-
 function startClock() {
-    intervalId = setInterval(setTime, 1000);
+    setInterval(setTime, 1000);
 }
 
 function setTime() {    
     const time = new Date().toTimeString().slice(0, 5);
     const element = document.getElementById("clock-div");
     element.textContent = time;
-}
-
-function stopClock() {
-    if(intervalId !== -1) {
-        clearInterval(intervalId);
-    }
 }
