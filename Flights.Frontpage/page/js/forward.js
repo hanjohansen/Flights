07b52d3f6300580e3119251ab6febbf1562263af
Hanjo <hanjos.brainwash@googlemail.com>
2026-01-14 21:56:39 +0100

    added landing page project

---
+++ Flights.Frontpage/page/js/forward.js
@@ -0,0 +1,6 @@
+
+const fwdDiv = document.getElementById('fwd-btn');
+
+fwdDiv.addEventListener('click', function() {
+  window.open("https://play.flights-app.net","_self")
+});
---
+++ Flights.Frontpage/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+theme: {
+    extend: {
+      colors: {
+        'dark-background': '#010409',        
+        'dark-surface': '#212427',
+        'dark-border': '#373a3e',
+      },
+    },
+  },
+  plugins: [],
+}
